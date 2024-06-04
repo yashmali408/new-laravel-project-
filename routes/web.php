b20772ad6ba92dcd3bc63e0e8851d565e6bcb8e8
anildalar <dollorinfotech@gmail.com>
2024-06-04 10:11:24 +0530

    404 route added

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,31 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/shop/cart',function(){
-    return view('shop/cart');
+Route::prefix('/shop')->group(function () {
+    Route::get('/cart',function(){
+        return view('shop/cart');
+    });
+    Route::get('/shop-grid',function(){
+        return view('shop/shop-grid'); //shop-grid.blade.php
+    });
+    Route::get('/my-account',function(){
+        return view('shop/my-account'); //my-account.blade.php
+    });
+    Route::get('/my-account',function(){
+        return view('shop/my-account'); //my-account.blade.php
+    });
+    Route::get('/track-your-order',function(){
+        return view('shop/track-your-order'); //my-account.blade.php
+    });
+    Route::get('/compare',function(){
+        return view('shop/compare'); //compare.blade.php
+    });
+    Route::get('/shop',function(){
+        return view('shop/shop'); //shop.blade.php
+    });
 });
-Route::get('/shop/shop-grid',function(){
-    return view('shop/shop-grid'); //shop-grid.blade.php
+
+Route::get('/faq',function(){
+    return view('faq'); //faq.blade.php
 });
+
