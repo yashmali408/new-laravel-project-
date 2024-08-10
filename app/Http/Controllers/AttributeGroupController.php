7caa9602f7729e957c10b4f190e701941e521ab7
anildalar <dollorinfotech@gmail.com>
2024-08-10 10:22:59 +0530

    new

---
+++ app/Http/Controllers/AttributeGroupController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttributeGroup;
+use Illuminate\Http\Request;
+
+class AttributeGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AttributeGroup $attributeGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AttributeGroup $attributeGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AttributeGroup $attributeGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AttributeGroup $attributeGroup)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_10_042207_create_attribute_groups_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attribute_groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag_id');
+            $table->string('attribute_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attribute_groups');
+    }
+};
