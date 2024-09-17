e30d48e90d4ae5f968abb88d8619273da35ce32d
anildalar <dollorinfotech@gmail.com>
2024-09-17 09:55:53 +0530

    Some error while i laucnh plz fix it

--- socket-server.js
+++ socket-server.js
@@ -4,15 +4,16 @@ import { Server } from 'socket.io';
 const io = new Server(3000);
 
 // Print that the socket server is running
-console.log('Socket server is running on port 3000');
+console.log('Socket server is running on port 3000 Anil');
 
 //on = listen
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    //on = listen
+    //on = listen = recieve
     socket.on('chat:message', (message) => {
-        // emit = speak
+        // emit = speak = send
+        //We are sending some informattion
         io.emit('chat:message', message);
     });
 
